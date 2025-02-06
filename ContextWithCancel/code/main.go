420d40e12b0b67bdf86f4101e14905f6f7402076
cl352v_wayfair <clopezlerma@wayfair.com>
2025-02-06 11:30:58 +0100

    Context with Cancel

--- ContextWithCancel/code/main.go
+++ ContextWithCancel/code/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+)
+
 func main() {
-	
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func(ctx context.Context) {
+		defer wg.Done()
+		for range time.Tick(500 * time.Millisecond) {
+			if ctx.Err() != nil {
+				log.Println(ctx.Err())
+				return
+			}
+			println("tick!")
+		}
+	}(ctx)
+
+	time.Sleep(5 * time.Second)
+	cancel()
+
+	wg.Wait()
+
 }
