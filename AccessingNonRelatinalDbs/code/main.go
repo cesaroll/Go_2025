e4f823c8d3b336d0d86c71cb0f33bb1f8d2068c6
cl352v_wayfair <clopezlerma@wayfair.com>
2025-02-12 15:20:58 +0100

    inserted many

--- AccessingNonRelatinalDbs/code/main.go
+++ AccessingNonRelatinalDbs/code/main.go
@@ -20,6 +20,7 @@ func main() {
 	}
 	defer mongoDb.DisconnectMongo(client) // Ensures disconnection when the program exits
 
+	// Insert One
 	tom := actor{"Tom", "Hanks", 9}
 	actorsCollection := getActorsCollection(client)
 	insertResult, err := actorsCollection.InsertOne(context.TODO(), tom)
@@ -27,5 +28,15 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted new Actor: ", insertResult.InsertedID)
+
+	// Insert Many
+	mark := actor{"Mark", "Hamil", 2}
+	mili := actor{"Mili", "Bobby Brown", 3}
+	actors := []interface{}{mark, mili}
+	insertManyResult, err := actorsCollection.InsertMany(context.TODO(), actors)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted multiple Actors: ", insertManyResult.InsertedIDs)
 
 }
