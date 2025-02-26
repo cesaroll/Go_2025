c6d99e130fa58d9363c382991ce49df9fe591575
cl352v_wayfair <clopezlerma@wayfair.com>
2025-02-26 22:07:10 +0100

    Add application type and a method for it in handlers file

--- ReadingListApi/cmd/api/main.go
+++ ReadingListApi/cmd/api/main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", healthcheck)
+const version = "1.0.0"
 
-	err := http.ListenAndServe(":8040", mux)
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8040, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|port)")
+	flag.Parse()
+
+	app := &application{
+		config: cfg,
+		logger: log.New(log.Writer(), "API: ", log.Ldate|log.Ltime),
+	}
+
+	addr := fmt.Sprintf(":%d", cfg.port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+	log.Printf("starting %s server on %s", cfg.env, addr)
+	err := http.ListenAndServe(addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func healthcheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", "dev")
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
 }
