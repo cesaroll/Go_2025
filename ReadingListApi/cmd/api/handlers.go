ff96b554deffbaa6d8e3bd18030877f93296543a
cl352v_wayfair <clopezlerma@wayfair.com>
2025-02-26 22:26:36 +0100

    Adding routes and handlers

--- ReadingListApi/cmd/api/handlers.go
+++ ReadingListApi/cmd/api/handlers.go
@@ -6,15 +6,31 @@ import (
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		return
+	}
+
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
 }
 
 func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "get all books")
+	}
+	if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "create a new book")
+	}
 }
 
 func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r *http.Request) {
-
+	switch r.Method {
+		case http.MethodGet:
+			fmt.Fprintln(w, "get a book")
+		case http.MethodPut:
+			fmt.Fprintln(w, "update a book")
+		case http.MethodDelete:
+			fmt.Fprintln(w, "delete a book")
+	}
 }
